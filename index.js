f8c30b39b57091b5e77c4333ca918a70c6a33f7b
AlexandraMarkova <a.markova@ukr.net>
2021-02-05 19:43:28 +0200

    добавляет сетИнтервал  и смену бекграунда на боди

---
+++ index.js
@@ -0,0 +1,40 @@
+const colors = [
+    '#FFFFFF',
+    '#2196F3',
+    '#4CAF50',
+    '#FF9800',
+    '#009688',
+    '#795548',
+];
+
+const refs = {
+    buttonStartRef: document.querySelector('button[data-action="start"]'),
+    buttonStopRef: document.querySelector('button[data-action="stop"]'), 
+}
+
+refs.buttonStartRef.addEventListener("click", randomColorsStart)
+refs.buttonStopRef.addEventListener("click", randomColorsStop)
+
+ 
+let intervalId = null;
+
+function randomColorsStart() {
+    refs.buttonStartRef.removeEventListener("click", randomColorsStart);
+
+    const randomIntegerFromInterval = (min, max) => {
+        return Math.floor(Math.random() * (max - min + 1) + min);
+    };
+
+    intervalId = setInterval(() => {
+        const color = colors.find((value, idx) => {
+        return idx === randomIntegerFromInterval(idx, colors.length - 1)
+    });
+    // console.log(color)
+    document.body.style.background = color;
+}, 1000)
+}
+
+function randomColorsStop() { 
+    refs.buttonStartRef.addEventListener("click", randomColorsStart)
+    clearInterval(intervalId);
+}
